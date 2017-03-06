fa26b6593c51babd5a83fb6c9726557966c94857
Asep Saepudin <asepsaepudin@msn.com>
2017-03-07 00:50:21 +0700

    Update username/password configuration

--- src/main/scala/io/github/asepsaep/Repository.scala
+++ src/main/scala/io/github/asepsaep/Repository.scala
@@ -3,6 +3,7 @@ package io.github.asepsaep
 import reactivemongo.api.MongoDriver
 import reactivemongo.api.collections.bson.BSONCollection
 import reactivemongo.bson.{BSONDocument, Macros}
+import reactivemongo.core.nodeset.Authenticate
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -11,7 +12,7 @@ class Repository(implicit ec: ExecutionContext) extends Config {
   private implicit val userHandler = Macros.handler[User]
 
   private val driver = new MongoDriver
-  private val connection = driver.connection(List(mongoHost))
+  private val connection = driver.connection(List(mongoHost), authentications = List(Authenticate(mongoDb, mongoUser, mongoPassword)))
   private val users: Future[BSONCollection] = connection.database(mongoDb).map(db => db.collection("users"))
 
   def insertUser(user: User): Future[User] = {
